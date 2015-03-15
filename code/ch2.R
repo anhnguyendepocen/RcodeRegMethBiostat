87f9e34841b7d53e4300c0e33406a85185ee8970
cmlakhan <chirag.lakhani@gmail.com>
2015-03-15 17:59:52 -0400

    chapter 2 updates
    summary statistics for sbp

--- code/ch2.R
+++ code/ch2.R
@@ -1,2 +1,12 @@
 wcgs <- read.csv("../RcodeRegMethBiostat//data/wcgs.csv")
+install.packages("ggplot2")
 
+library(ggplot2)
+
+
+#Summary Statistics of systolic blood pressure (sbp)
+summary(wcgs$sbp)
+
+ggplot(wcgs, aes(x=sbp)) + geom_histogram(aes(y=..density..), fill="white", color="blue")  + geom_density(alpha=.2, fill="#FF6666")
+
+boxplot(wcgs$sbp)
