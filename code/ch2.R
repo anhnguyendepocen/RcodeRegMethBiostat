279233237157a56f6ed410f0b4e03549d6165a91
cmlakhan <chirag.lakhani@gmail.com>
2015-03-15 18:08:49 -0400

    update to plots

--- code/ch2.R
+++ code/ch2.R
@@ -7,6 +7,12 @@ library(ggplot2)
 #Summary Statistics of systolic blood pressure (sbp)
 summary(wcgs$sbp)
 
+#Histogram
 ggplot(wcgs, aes(x=sbp)) + geom_histogram(aes(y=..density..), fill="white", color="blue")  + geom_density(alpha=.2, fill="#FF6666")
 
+#Boxplot
 boxplot(wcgs$sbp)
+
+#QQPlot for Normality
+qqnorm(wcgs$sbp)
+qqline(wcgs$sbp)
